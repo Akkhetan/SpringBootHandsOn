6d284b319c70b52ec5c5cf1a69fc702c6044ba7b
Akkhetan <89134783+Akkhetan@users.noreply.github.com>
2024-12-06 17:11:50 +0530

    Initial Commit

---
+++ SpringDataJPA/taco-cloud-jpa/src/main/java/com/prac/taco_cloud_jpa/TacoCloudJpaApplication.java
@@ -0,0 +1,13 @@
+package com.prac.taco_cloud_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudJpaApplication.class, args);
+	}
+
+}
