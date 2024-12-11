20f3d7e31bb41dd206bb43eb297fdee71f02af38
Ankit <akkhetan28@gmail.com>
2024-12-11 15:58:34 +0530

    Spring Security Step2 : Declare PasswordEncoder bean

---
+++ SpringSecurity/taco-cloud-ss/src/main/java/com/prac/taco_cloud_ss/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.prac.taco_cloud_ss.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
