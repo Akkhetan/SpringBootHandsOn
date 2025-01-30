4848d305ba8bb9f05cf395c370248970aaa5cc68
Ankit <akkhetan28@gmail.com>
2025-01-30 18:34:07 +0530

    Spring Rest : Initial Commit

---
+++ SpringREST/taco-cloud-rest/src/test/java/com/prac/taco_cloud_rest/TacoCloudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.taco_cloud_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
